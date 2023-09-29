d9ffa4b2f0be7d97921ac989b775fc359d86ed54
tanii-shio <naoto_shioya@IBM-PF44N5GX.localdomain>
2023-09-29 14:42:50 +0900

    chat bot demo

--- src/gradio_app.py
+++ src/gradio_app.py
@@ -1,8 +1,20 @@
+import random
+import time
+ 
 import gradio as gr
-
-def greet(name):
-    return "Hello " + name + "!"
-
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-    
+ 
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.Button("Clear")
+ 
+    def respond(message, chat_history):
+        bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
+        chat_history.append((message, bot_message))
+        time.sleep(1)
+        return "", chat_history
+ 
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+    clear.click(lambda: None, None, chatbot, queue=False)
+ 
 demo.launch()
