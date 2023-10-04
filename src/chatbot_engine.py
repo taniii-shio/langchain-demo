cadc1c3c5faee901cc388e4b766c865bd1b360dc
tanii-shio <naoto_shioya@IBM-PF44N5GX.localdomain>
2023-10-04 14:01:44 +0900

    implement index

--- src/chatbot_engine.py
+++ src/chatbot_engine.py
@@ -2,13 +2,18 @@ from langchain.chat_models import ChatOpenAI
 import langchain
 from langchain.memory import ChatMessageHistory
 from langchain.schema import HumanMessage
+from langchain.document_loaders import DirectoryLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 
 langchain.verbose = True
 
-def chat(message: str, history: ChatMessageHistory) -> str:
+def create_index() -> VectorStoreIndexWrapper:
+  loader = DirectoryLoader("./src/", glob="**/*.py")
+  return VectorstoreIndexCreator().from_loaders([loader])
+  
+
+def chat(message: str, history: ChatMessageHistory, index: VectorStoreIndexWrapper) -> str:
   llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
-  messages = history.messages
-  messages.append(HumanMessage(content=message))
-
-  return llm(messages).content
+  return index.query(question=message, llm=llm)
